a4973810a0dcb31fe1ca8dfc07c784fb18477c62
Catherine Smith <catsmith24601@gmail.com>
2021-05-07 10:56:37 -0400

    played withposting new note - still need to finish checking

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,7 +2,7 @@
 // const path = require('path');
 
 // Load data - linking routes to data sources, which hold (arrays - possibly? - that's what db.json looks like - title and text in note array) of information on notes
-const { response } = require('express');
+const { response, request } = require('express');
 const noteData = require('../db/db.json');
 // const uuid = require('uuid');
 // OR is the uuid part like this from https://www.geeksforgeeks.org/node-js-npm-uuid/
@@ -14,21 +14,49 @@ const { v4 : uuidv4 } = require('uuid');
 module.exports = (app) => {
     // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
     // Displays saved api view of notes - function will send json data of the notes upon request
-    app.get('/api/notes', (req, res) => res.json(notes));
+    app.get('/api/notes' = (req, res) => {
+        fs.readFile('./db/db.json', (err, data) => {
+            // throw error if an error occurs
+            if (err) throw err;
+            // otherwise parse the json data
+            else {res.json(JSON.parse(data))};
+        });
+    };
 
     // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
     // and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved 
     // (look into `npm` packages that could do this for you).
-    // ****uuid = creates unique ID
+    // ****uuid = creates unique ID = Universally Unique ID
 
     // Function to actually POST the data to the database (database here is db.json)
     app.post('/api/notes', (req, res) => {
 
-        // Need to first add an object to the notes array
-        const newNote = request.body
+        // Need to first add an object to the notes array - using user input
+        const newNote = {
+            title: request.body.title,
+            text: request.body.text,
+            // check that this is the right way to write it in this instance 
+            id: uuidv4(),
+        };
+         
+        // will need to go back and forth from JSON to array
+        fs.readFile('../db/db.json', (err, data) => {
+            // remember: parse() takesa json string and makes it a js object, and stringify() takes a js onject and makes it a json string
+            const notes = JSON.parse(data)
+            
 
-        // Then need to push that object to the notes array
+            // Then need to push that object to the notes array
+            // push the new note to the array
+            notes.push(newNote);
 
+            // change back to json and add to db.json file
+            fs.writeFile('../db/db.json'), JSON.stringify(notes) = (err, res) => {
+                // if an error occurs, throw an error
+                if (err) throw err;
+                 // otherwise parse the json data
+                else {res.send(newNote)};
+            };   
+        })
         // Then end
         response.end();
     });
