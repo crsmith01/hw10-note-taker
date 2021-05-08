bfe28f5bcd0b96e8c9cbaf4f8fdcd8a39f7935aa
Catherine Smith <catsmith24601@gmail.com>
2021-05-08 17:37:10 -0400

    fixed link in htmlRoutes.js

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,11 +4,11 @@ const path = require('path');
 // Routes - html GET requests - each sends the specified html file upon request
 module.exports = (app) => {
     // for homepage
-    app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
     
     // for notes page
-    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
     
     // Redirects to homepage if user types in anything aside from the two aforementioned paths
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-}
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+};
